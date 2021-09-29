9b24e4ac2c19df962a363b10c1fc2e8dcf3f896b
Rustic2 <rus5i1819@gmail.com>
2021-09-29 20:27:10 +0500

    test master.1

--- src/main/java/com/baratov/spring/springboot/controller/AdminController.java
+++ src/main/java/com/baratov/spring/springboot/controller/AdminController.java
@@ -18,7 +18,7 @@ import java.util.Set;
 public class AdminController {
 
     private IUserService userService;
-//test master
+//test master.1
     @Autowired
     public AdminController(IUserService userService) {
         this.userService = userService;
